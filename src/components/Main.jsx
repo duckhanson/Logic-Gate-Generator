e802c81f101213cecf043ce4b1fe4e48713e03a7
107060022 <ducklyu0301@gmail.com>
2021-06-04 14:52:40 +0800

    enable case sensitive

---
+++ src/components/Main.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
+import "./Main.css";
+import HomePage from "./HomePage";
+import WorkPage from "./WorkPage";
+
+const Main = (props) => {
+    return (
+        <Router>
+            <div className='Main'>
+                <Container className="bg-faded">
+                    <Row>
+                        <Col>
+                            <Route exact path="/" component={HomePage} />
+                        </Col>
+                    </Row>
+                </Container>
+                <Route path="/WorkPage" component={WorkPage}/>
+            </div>
+        </Router>
+    );
+};
+
+export default Main;
---
+++ src/components/TruthTable.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import './TruthTable.css';
+
+const TruthTable = (props) => {
+    
+    return (
+        <div>
+            <h1>I'm TruthTable</h1>
+        </div>
+    );
+}
+ 
+export default TruthTable;
