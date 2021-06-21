b896be3331f086d70ea895748d6a69bbdbbfa324
duck <ducklyu0301@gmail.com>
2021-06-21 08:11:20 +0000

    TODO: logic gate diagram svg display in Main.jsx

---
+++ src/components/Result.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Result = (props) => {
+    return (
+        <div>
+            {/* Main */}
+            <div id="main">
+                <section className="post">
+                    {/* <img src={props.resultFilePath} alt="this is the result of logic diagram" width='500' height='500' /> */}
+                </section>
+            </div>
+        </div>
+    );
+};
+
+export default Result;
